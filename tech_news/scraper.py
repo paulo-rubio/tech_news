c0cd1ace22287a75c9992d0c26d3878eb20dbdb3
Rubio <paulocezarrubio@gmail.com>
2023-03-17 11:24:36 -0300

    req1-3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,16 +1,39 @@
 # Requisito 1
+import requests
+import time
+
+from parsel import Selector
+
+# from tech_news.database import create_news
+
+
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        html = requests.get(
+            {"url": url, "timeout": 3, "user-agent": "Fake user-agent"}
+        )
+
+        time.sleep(1)
+
+        if html.status_code != 200:
+            return None
+        return html.text
+    except requests.exceptions.RequestException:
+        return None
 
 
 # Requisito 2
 def scrape_updates(html_content):
-    """Seu código deve vir aqui"""
+    data = Selector(html_content).css(".cs-overlay-link::attr(href)").getall()
+    return data
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    data = Selector(html_content).css(".next::attr(href)").get()
+    if not data:
+        return None
+    return data
 
 
 # Requisito 4
