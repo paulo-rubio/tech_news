dd64bb82ecc0310b4ee5a7b72f845614c0f18b6d
Rubio <paulocezarrubio@gmail.com>
2023-03-19 14:21:39 -0300

    req7_8

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,23 @@
+from datetime import datetime
+from tech_news.database import search_news
+
+
 # Requisito 7
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui""" ""
+    data_news = search_news({"title": {"$regex": title, "$options": "$i"}})
+    return [(new["title"], new["url"]) for new in data_news]
 
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        date = datetime.strptime(date, "%Y-%m-d").strftime("%d/%m/%Y")
+        data_news = search_news({"timestamp": date})
+        return [(new["title"], new["url"]) for new in data_news]
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
