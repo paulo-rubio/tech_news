097a33f09adc396d4acde2cc8d66abf5c02e564f
Rubio <paulocezarrubio@gmail.com>
2023-03-19 14:26:24 -0300

    fix8

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -13,7 +13,7 @@ def search_by_title(title):
 # Requisito 8
 def search_by_date(date):
     try:
-        date = datetime.strptime(date, "%Y-%m-d").strftime("%d/%m/%Y")
+        date = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
         data_news = search_news({"timestamp": date})
         return [(new["title"], new["url"]) for new in data_news]
     except ValueError:
